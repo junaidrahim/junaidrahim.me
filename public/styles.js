22ef532dd704c4685128e0b20fb0c3b6ad47fbac
junaidrahim <junaidrahim8d@gmail.com>
2018-12-21 17:54:48 +0530

    Fixed the font error and adjusted the icon size

--- public/styles.js
+++ public/styles.js
@@ -555,7 +555,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/junaid/Code/test/junaidrahim.me/frontend/src/styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! /home/junaid/Code/junaidrahim.me/frontend/src/styles.css */"./src/styles.css");
 
 
 /***/ })
