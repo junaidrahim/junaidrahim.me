eae2600d950caf2fa951607ffdf0ca04b54551ff
junaidrahim <junaidrahim8d@gmail.com>
2018-12-22 13:43:33 +0530

    Converted contact part from px to rem

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { ResumeComponent } from './resume/resume.component';
 import { BlogComponent } from './blog/blog.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
     ResumeComponent,
-    BlogComponent
+    BlogComponent,
+    TechnologiesComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
