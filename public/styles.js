df58aca375c4dc028249bb9f57be1b3c790e46b9
JunaidRahim <junaidrahim8d@gmail.com>
2019-01-13 00:03:06 +0530

    Built the latest version

--- public/styles.js
+++ public/styles.js
@@ -555,7 +555,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/junaid/Code/junaidrahim.me/frontend/src/styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! /home/junaidrahim/Code/Github-Projects/junaidrahim.me/frontend/src/styles.css */"./src/styles.css");
 
 
 /***/ })
