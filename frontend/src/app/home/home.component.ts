c4fcecdfbabdd8043a1f408dad9d564cf058ffb5
junaidrahim <junaidrahim8d@gmail.com>
2019-03-15 17:25:39 +0530

    Halfway through home navbar, need to add responsie behaviour

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -9,7 +9,34 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  
   ngOnInit() {
+    
+  }
+
+  go_aboutme(){
+    let e = document.getElementById("about_me");
+    e.scrollIntoView({ block: 'start',  behavior: 'smooth' });
+  }
+
+  go_skills(){
+    let e = document.getElementById("skills");
+    e.scrollIntoView({ block: 'start',  behavior: 'smooth' });
+  }
+
+  go_projects(){
+    let e = document.getElementById("projects");
+    e.scrollIntoView({ block: 'start',  behavior: 'smooth' });
+  }
+
+  go_blog(){
+    let e = document.getElementById("my_blog");
+    e.scrollIntoView({ block: 'start',  behavior: 'smooth' });
+  }
+
+  go_contact(){
+    let e = document.getElementById("contact");
+    e.scrollIntoView({ block: 'start',  behavior: 'smooth' });
   }
 
 }
