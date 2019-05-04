7cd2a018f8ffd023593a8eae7047fabf4f56d66a
JunaidRahim <junaidrahim8d@gmail.com>
2019-05-04 11:38:04 +0530

    Changed Skills and Blog section, Added the Intro part

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
-import { ResumeComponent } from './resume/resume.component';
 import { BlogComponent } from './blog/blog.component';
 import { TechnologiesComponent } from './technologies/technologies.component';
 import { ProjectsComponent } from './projects/projects.component';
@@ -16,7 +15,6 @@ import { HomeComponent } from './home/home.component';
   declarations: [
     AppComponent,
     ContactComponent,
-    ResumeComponent,
     BlogComponent,
     TechnologiesComponent,
     ProjectsComponent,
