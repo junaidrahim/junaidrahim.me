6d1c5ecb39bc859277e6bd3d3df4f0a30e24f003
Junaid Rahim <junaidrahim8d@gmail.com>
2020-05-20 17:12:12 +0530

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+echo -e "\e[1;34mjunaidrahim.me - Portfolio Website\e[0m"
+echo -e "\e[1;32mDeploying to junadirahim.github.io\e[0m"
+echo "..."
+
+echo "Removing Files"
+rm -rf ../junaidrahim.github.io/*
+echo "junaidrahim.me" > ../junaidrahim.github.io/CNAME
+cp -r public/* ../junaidrahim.github.io/
+
+echo -e "\e[1;32mStarting Commit and Push\e[0m"
+cd ../junaidrahim.github.io && git add . && git commit -m "latest deploy" && git push origin master
